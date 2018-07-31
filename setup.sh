ccbf2ffff846100e64674705a54a6fda7c96447c
luca <lp@inser.ch>
2018-07-31 14:28:28 +0200

    Allow access from anywhere

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ CONF="/etc/postgresql/9.3/main/postgresql.conf"
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 # And allow access from DockerToolbox / Boottodocker on OSX
 echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+# And allow access from anywhere
+echo "host    all             all             0.0.0.0/0               md5" >> /etc/postgresql/9.3/main/pg_hba.conf
 
 # Listen on all ip addresses
 echo "listen_addresses = '*'" >> /etc/postgresql/9.3/main/postgresql.conf
